c107931ffb47e05aee6cf1bfce3d6d8a29f1c682
Martin <svobodamartin@centrum.cz>
2024-07-12 14:32:55 +0200

    Header design updated

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,20 +5,21 @@ import styled from "styled-components";
 const StyledDiv = styled.header`
     display: flex;
     justify-content: space-between;
+    align-items: center;
 
     button {
         display: flex;
+
         justify-content: center;
         align-items: center;
     
         cursor: pointer;
 
-        all: unset;
-
         width: 35px;
         height: 35px;
 
         background-color: ${(props) => props.theme.colors.grass9};
+        border: 0;
         border-radius: 50%;
 
         color: #fff;
