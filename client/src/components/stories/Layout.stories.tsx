0924038a013561a65c81d530a98dcbd08d635e12
Martin <svobodamartin@centrum.cz>
2024-07-13 19:28:48 +0200

    SB2: Button variants story added

--- client/src/components/stories/Layout.stories.tsx
+++ client/src/components/stories/Layout.stories.tsx
@@ -8,9 +8,6 @@ import { Footer } from "../Footer";
 const meta = {
     title: "Layout",
     component: Layout,
-    argTypes: {
-        onItemAdd: { action: "item added" },
-    },
 } as Meta<typeof Layout>;
 export default meta;
 type Story = StoryObj<typeof Layout>;
